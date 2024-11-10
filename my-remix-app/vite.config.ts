a0cebcbd58ed835aaf9a1fbdaf35afb6bffd137a
bz0 <bzz0217@yahoo.co.jp>
2024-11-10 15:20:04 +0900

    Add Codecov bundler plugin

--- my-remix-app/vite.config.ts
+++ my-remix-app/vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { codecovRemixVitePlugin } from "@codecov/remix-vite-plugin";
 
 declare module "@remix-run/node" {
   interface Future {
@@ -20,5 +21,10 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    codecovRemixVitePlugin({
+      enableBundleAnalysis: true,
+      bundleName: "example-remix-bundle",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
   ],
 });
